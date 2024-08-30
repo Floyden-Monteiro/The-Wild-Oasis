1696eba822b21ca1fcd1ef617026c94fddada97c
Floyden-Monteiro <floydensocialmedia@gmail.com>
2024-08-30 07:53:12 +0530

    Add: initial project files and configurations ✨

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://uetsfabnpkicudxbqczh.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVldHNmYWJucGtpY3VkeGJxY3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NDYwNDYsImV4cCI6MjA0MDQyMjA0Nn0.b1ZE0PazIG15nxES5RICz2j0jrz9rPPZ7B8a2M2JvRQ';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
