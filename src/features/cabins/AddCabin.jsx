93051a034312ac45b4ad110cff4bad23533ba8fa
Floyden-Monteiro <floydensocialmedia@gmail.com>
2024-09-02 12:20:11 +0530

    Refactor: AddCabin component to use new Modal 🚀

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,18 +1,37 @@
-import Button from 'ui/Button';
-import Modal from 'ui/Modal';
+
+
+import Button from '../../ui/Button';
+import Modal from '../../ui/Modal';
 import CreateCabinForm from './CreateCabinForm';
 
 function AddCabin() {
   return (
     <Modal>
-      <Modal.Toggle opens='new-cabin'>
+      <Modal.Open opens='cabin-form'>
         <Button>Add new cabin</Button>
-      </Modal.Toggle>
-      <Modal.Window name='new-cabin'>
+      </Modal.Open>
+      <Modal.Windoow name='cabin-form'>
         <CreateCabinForm />
-      </Modal.Window>
+      </Modal.Windoow>
     </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onClose={() => setIsOpenModal(false)}/>
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
